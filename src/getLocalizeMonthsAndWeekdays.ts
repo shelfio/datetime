462cb82bb68aee0021df3a9912f51957329c003f
Yaroslav Chapelskyi <chapelskyi.slavik@gmail.com>
2023-07-04 11:22:11 +0300

    fix: initialize localeData in function so locales are always up-to-date

--- src/getLocalizeMonthsAndWeekdays.ts
+++ src/getLocalizeMonthsAndWeekdays.ts
@@ -2,19 +2,18 @@ import dayjs from 'dayjs';
 import localeDataPlugin from 'dayjs/plugin/localeData';
 
 dayjs.extend(localeDataPlugin);
-const localeData = dayjs().localeData();
 
 export default () => {
+  const localeData = dayjs().localeData();
+
   const monthNumbers = Array.from({length: 12}, (_, i) => i);
   const weekdaysNumbers = Array.from({length: 7}, (_, i) => i);
 
   return {
-    monthsShort: monthNumbers.map(month => localeData.monthsShort()[month]).filter(Boolean),
-    monthsFull: monthNumbers.map(month => localeData.months()[month]).filter(Boolean),
-    weekdaysFull: weekdaysNumbers.map(day => localeData.weekdays()[day]).filter(Boolean),
-    weekdaysAbbreviated: weekdaysNumbers
-      .map(day => localeData.weekdaysShort()[day])
-      .filter(Boolean),
-    weekdaysShort: weekdaysNumbers.map(day => localeData.weekdaysMin()[day]).filter(Boolean),
+    monthsShort: monthNumbers.map(month => localeData.monthsShort()[month]),
+    monthsFull: monthNumbers.map(month => localeData.months()[month]),
+    weekdaysFull: weekdaysNumbers.map(day => localeData.weekdays()[day]),
+    weekdaysAbbreviated: weekdaysNumbers.map(day => localeData.weekdaysShort()[day]),
+    weekdaysShort: weekdaysNumbers.map(day => localeData.weekdaysMin()[day]),
   };
 };
