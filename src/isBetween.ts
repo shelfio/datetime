38117fba594e99dddcb086b9623dde23db4a7ee5
yuriiLevantovych <domovoj1@gmail.com>
2024-11-19 10:08:21 +0200

    feat: CPWAPI-1125 add isBetween and addYears

---
+++ src/isBetween.ts
@@ -0,0 +1,10 @@
+import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
+
+dayjs.extend(isBetween);
+
+export default (
+  dateLeft: dayjs.ConfigType,
+  dateMiddle: dayjs.ConfigType,
+  dateRight: dayjs.ConfigType
+): boolean => dayjs(dateMiddle).isBetween(dayjs(dateLeft), dayjs(dateRight));
